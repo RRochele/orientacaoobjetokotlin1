edd4d8a9b77ccd8dff9477c3878bc708627cef2a
rochele <rrochelefsd@gmail.com>
2022-04-03 22:57:10 -0300

    First steps in Object Orientation

--- Pessoa.kt
+++ Pessoa.kt
@@ -1,8 +1,37 @@
 package kotlindigitalinnovation
 
 fun main() {
-    var i = 0
+    println("Bem vindo ao Bank")
+    val contaRochele = Conta()
+    contaRochele.titular = "Rochele"
+    contaRochele.numero = 1000
+    contaRochele.saldo = 200.0
 
+
+    val contaJoaquim = Conta()
+    contaJoaquim.titular = "Joaquim"
+    contaJoaquim.numero = 1001
+    contaJoaquim.saldo = 300.0
+
+    println(contaRochele.titular)
+    println(contaRochele.numero)
+    println(contaRochele.saldo)
+    println()
+
+    println(contaJoaquim.titular)
+    println(contaJoaquim.numero)
+    println(contaJoaquim.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+}
+
+fun testaLacos() {
+    var i = 0
     while (i < 5) {
         val titular: String = "Rochele $i"
         val numeroConta: Int = 1000 + i
@@ -15,7 +44,6 @@ fun main() {
         i++
 
     }
-
 }
 
 //    for (i in 5 downTo 1) {
@@ -33,23 +61,24 @@ fun main() {
 //        testaCondicoes(saldo)
 
 
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("conta é positiva")
-    } else if (saldo == 0.0) {
-        println("conta é neutra")
-    } else {
-        println("conta é negativa")
-    }
+//fun testaCondicoes(saldo: Double) {
+//    if (saldo > 0.0) {
+//        println("conta é positiva")
+//    } else if (saldo == 0.0) {
+//        println("conta é neutra")
+//    } else {
+//        println("conta é negativa")
+//    } }
 
-    // outra maneira usando when
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        // condição contraria:verificar se é falso e em seguida verificar se é o zero
-        else -> println("conta é negativa")
-    }
-}
+
+//    // outra maneira usando when
+//    when {
+//        saldo > 0.0 -> println("conta é positiva")
+//        saldo == 0.0 -> println("conta é neutra")
+//        // condição contraria:verificar se é falso e em seguida verificar se é o zero
+//        else -> println("conta é negativa")
+//    }
+
 
 
 
