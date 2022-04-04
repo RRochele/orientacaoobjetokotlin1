5d6683044ffceb1e071f76b1ba15d4f0284e7028
rochele <rrochelefsd@gmail.com>
2022-04-03 22:31:29 -0300

    Criated Class Pessoa test if and when test for loop and while

--- Pessoa.kt
+++ Pessoa.kt
@@ -1,14 +1,55 @@
 package kotlindigitalinnovation
 
-class Pessoa (
-  var nome: String= "Rochele",
-  var cpf: String= "124.571.645-50"
+fun main() {
+    var i = 0
 
-        )
+    while (i < 5) {
+        val titular: String = "Rochele $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-fun main(args: Array<String>) {
-    val Rochele= Pessoa()
+        println("titular $titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
 
-    println(Rochele.nome)
-    println(Rochele.cpf)
+    }
+
 }
+
+//    for (i in 5 downTo 1) {
+//        while ()
+//        }
+//        val titular: String = "Rochele $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 10.0
+//
+//
+//        println("titular $titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//        testaCondicoes(saldo)
+
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    // outra maneira usando when
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        // condição contraria:verificar se é falso e em seguida verificar se é o zero
+        else -> println("conta é negativa")
+    }
+}
+
+
+
