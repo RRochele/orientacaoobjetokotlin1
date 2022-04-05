66fa6ada07592fcf12c08e87793eb66233780ec8
rochele <rrochelefsd@gmail.com>
2022-04-04 22:32:37 -0300

    Criated Class and Object copy and reference implementing deposit implementing the methods

--- Pessoa.kt
+++ Pessoa.kt
@@ -21,64 +21,125 @@ fun main() {
     println(contaJoaquim.titular)
     println(contaJoaquim.numero)
     println(contaJoaquim.saldo)
+
+    println("depositando na conta da Rochele")
+    contaRochele.deposita(50.0)
+    println(contaRochele.saldo)
+
+    println("depositando na conta da Joaquim")
+    contaJoaquim.deposita(70.0)
+    println(contaJoaquim.saldo)
+
+    println("sacando na conta da Rochele")
+    contaRochele.saca(250.0)
+    println(contaRochele.saldo)
+
+    println("sacando na conta de Joaquim")
+    contaJoaquim.saca(100.0)
+    println(contaJoaquim.saldo)
+
+    println("saque em excesso na conta da Rochele")
+    contaRochele.saca(100.0)
+    println(contaRochele.saldo)
+
+    println("saque em excesso na conta do Joaquim")
+    contaJoaquim.saca(500.0)
+    println(contaJoaquim.saldo)
+
+
 }
+
 
 class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
 
-}
+    fun deposita(valor: Double) {
+        //this: vc esta mexendo em alguma coisa do próprio objeto
+        this.saldo += valor
+    }
 
-fun testaLacos() {
-    var i = 0
-    while (i < 5) {
-        val titular: String = "Rochele $i"
-        val numeroConta: Int = 1000 + i
-        var saldo = i + 10.0
-
-        println("titular $titular")
-        println("numero da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-        i++
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
 
     }
 }
-
-//    for (i in 5 downTo 1) {
-//        while ()
-//        }
+//
+//fun testaCopiasEReferencias() {
+//    // variaveis do tipo inteiro
+//
+//    val numeroX = 10
+//    var numeroY = numeroX
+//    numeroY++
+//    println("numeroX $numeroX")
+//    println("numeroY $numeroY")
+//
+//    // joao aponta para a conta e maria tambem.
+//    // lidar com objeto é lidar com referência
+//    val contaJoao = Conta()
+//    contaJoao.titular = "João"
+//    var contaMaria = Conta()
+//    contaMaria.titular = "Maria"
+//    println("titular conta joão : ${contaJoao.titular}")
+//    println("titular conta maria : ${contaMaria.titular}")
+//
+//    println(contaJoao)
+//    println(contaMaria)
+//
+//}
+//
+//fun testaLacos() {
+//    var i = 0
+//    while (i < 5) {
 //        val titular: String = "Rochele $i"
 //        val numeroConta: Int = 1000 + i
 //        var saldo = i + 10.0
-//
 //
 //        println("titular $titular")
 //        println("numero da conta $numeroConta")
 //        println("saldo da conta $saldo")
 //        println()
-//        testaCondicoes(saldo)
-
-
-//fun testaCondicoes(saldo: Double) {
-//    if (saldo > 0.0) {
-//        println("conta é positiva")
-//    } else if (saldo == 0.0) {
-//        println("conta é neutra")
-//    } else {
-//        println("conta é negativa")
-//    } }
-
-
-//    // outra maneira usando when
-//    when {
-//        saldo > 0.0 -> println("conta é positiva")
-//        saldo == 0.0 -> println("conta é neutra")
-//        // condição contraria:verificar se é falso e em seguida verificar se é o zero
-//        else -> println("conta é negativa")
+//        i++
+//
 //    }
-
-
-
-
+////}
+////
+////for (i in 5 downTo 1) {
+////    while ()
+////}
+////
+////val titular: String = "Rochele $i"
+////val numeroConta: Int = 1000 + i
+////var saldo = i + 10.0
+////
+////
+////println("titular $titular")
+////println("numero da conta $numeroConta")
+////println("saldo da conta $saldo")
+////println()
+////testaCondicoes(saldo)
+//
+//
+//fun testaCondicoes(saldo: Double) = if (saldo > 0.0) {
+//    println("conta é positiva")
+//} else if (saldo == 0.0) {
+//    println("conta é neutra")
+//} else {
+//    println("conta é negativa")
+//}
+//
+//
+//// outra maneira usando when
+////when {
+////    saldo > 0.0 -> println("conta é positiva")
+////    saldo == 0.0 -> println("conta é neutra")
+////    // condição contraria:verificar se é falso e em seguida verificar se é o zero
+////    else -> println("conta é negativa")
+////}
+//
+//
+//
+//
